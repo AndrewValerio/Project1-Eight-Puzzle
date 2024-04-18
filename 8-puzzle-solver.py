8ff73058d4d8cee36bb620cbf040b7506bcf48a2
AndrewValerio <andrewvalerio1396@gmail.com>
2024-04-17 21:21:44 -0700

    Added uniform cost search template

--- 8-puzzle-solver.py
+++ 8-puzzle-solver.py
@@ -29,6 +29,26 @@ class Problem:
 
 def uniform_cost_search(problem):
     # This method for the uniform cost search algorithm
+
+    # Template for function ----
+
+    # function UNIFORM-COST-SEARCH(problem) returns a solution, or failure
+    # node ←a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
+    # frontier ← a priority queue ordered by PATH-COST, with node as the only element
+    # explored ← an empty set
+    # loop do
+    # if EMPTY?(frontier ) then return failure
+    # node ← POP(frontier ) /* chooses the lowest-cost node in frontier */
+    # if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
+    # add node.STATE to explored
+    # for each action in problem.ACTIONS(node.STATE) do
+    # child ← CHILD-NODE(problem, node, action)
+    # if child.STATE is not in explored or frontier then
+    # frontier ← INSERT(child,frontier )
+    # else if child.STATE is in frontier with higher PATH-COST then
+    # replace that frontier node with child
+
+
     pass
 
 def a_star_search(problem, heuristic):
